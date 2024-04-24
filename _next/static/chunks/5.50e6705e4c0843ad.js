61b1c9cdff02371e9d7fe18c78dc0c2bbfa2ea91
mathewjordan <mat@northwestern.edu>
2024-04-24 13:59:49 +0000

    Deploying to gh-pages from @ nulib-ds/maktaba@c321b4bd144e24eea8cb4840e2401c23dcbeaa64 🚀

---
+++ _next/static/chunks/5.50e6705e4c0843ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5],{7005:function(s){s.exports=JSON.parse('[{"path":"/works","text":"Manuscripts"},{"path":"/essays","text":"Essays"},{"path":"/about","text":"About"}]')}}]);
